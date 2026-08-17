f8e80cbfef0e0bd99888741b2d67db5ee31631d0
Vinay <vinay@Vinays-MacBook-Air.local>
2026-08-17 09:57:26 +0530

    minor change

--- db/database.py
+++ db/database.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-load_dotenv()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
