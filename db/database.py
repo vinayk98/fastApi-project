20a70676896e738568e5b3d1fc90c339440b8f57
Vinay <vinay@Vinays-MacBook-Air.local>
2026-08-17 10:09:45 +0530

    configure database environment

--- db/database.py
+++ db/database.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(
